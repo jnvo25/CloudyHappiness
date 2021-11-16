d8d35f34db2aeb5e11454d7e86686579c86a2d13
fnyaung <fnyaung@gmail.com>
2021-11-15 17:22:48 -0800

    updated the a working (no design) navbar

--- shopify-react/src/components/App.js
+++ shopify-react/src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import ShopProvider from "../context/shopContext";
 import HomePage from "../pages/HomePage";
@@ -18,6 +18,14 @@ function App() {
     <ShopProvider>
       <StyletronProvider value={engine} debug={debug} debugAfterHydration>
         <BrowserRouter>
+          <ul>
+            <li>
+              <Link to="/">Cloudy Happiness</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/product/:id" element={<ProductPage />} />
