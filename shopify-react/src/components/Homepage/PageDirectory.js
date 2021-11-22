3c48eceae96609cfc44054bf0eec226196b3d91f
cagomez6 <64633124+cagomez6@users.noreply.github.com>
2021-11-22 11:19:07 -0800

    directory
    work in progress

--- shopify-react/src/components/Homepage/PageDirectory.js
+++ shopify-react/src/components/Homepage/PageDirectory.js
@@ -1,10 +1,27 @@
+import './PageDirectory.css';
 function PageDirectory() {
-  return (
-    // Place your implementation between these divs
-    <div>
-        <p>I am a page directory</p>
-    </div>
-  );
+    return (
+        // Place your implementation between these divs
+        <div id="directory">
+            <div id="shopNav" >
+                <button id="shopBtn">
+                    <a href="" class="linkText">Shop</a>
+                </button>
+            </div>
+
+            <div id="faqNav" >
+                <button id="faqBtn">
+                    <a href="" class="linkText">FAQ</a>
+                </button>
+            </div>
+
+            <div id="contactNav" >
+                <button id="contactBtn">
+                    <a href="" class="linkText">Contact</a>
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default PageDirectory;
