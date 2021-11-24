b635ab3eeee595390dc9d5f3b3e0613cd28d02ca
Jonathan Vo <jonatnv1@uci.edu>
2021-11-23 19:03:11 -0800

    Convert Atomize based cart to Bootstrap based

--- shopify-react/src/components/Homepage/Homepage.js
+++ shopify-react/src/components/Homepage/Homepage.js
@@ -3,7 +3,6 @@ import PageDirectory from './PageDirectory';
 import MyCarousel from './MyCarousel';
 import Footer from './Footer';
 
-
 function Homepage() {
   return (
     <div>
--- shopify-react/src/components/Navbar/Navbar.js
+++ shopify-react/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Container, Anchor, Icon } from "atomize";
+import { Anchor } from "atomize";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../context/shopContext";
 import { Nav, Navbar } from "react-bootstrap";
