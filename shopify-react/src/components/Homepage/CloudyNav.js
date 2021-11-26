07bb9c6d19649e569b0b182dfe985317ae232876
Jonathan Vo <jonatnv1@uci.edu>
2021-11-26 14:38:47 -0800

    Move cloud image down a little more

--- shopify-react/src/components/Homepage/CloudyNav.js
+++ shopify-react/src/components/Homepage/CloudyNav.js
@@ -5,7 +5,7 @@ import './CloudyNav.css';
 function CloudyNav() {
   return (
     <div>
-        <Image id="cloudy-image" src="https://i.ibb.co/3f5KZph/Header-Cloud.png" fluid />
+        <Image id="cloudy-image" src="https://i.ibb.co/4sftjPs/Header-Cloud.png" fluid />
     </div>
   );
 }
