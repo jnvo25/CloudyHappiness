bf437a777f2c560b85846d745176130c566b26c0
Jonathan Vo <jonatnv1@uci.edu>
2021-11-28 12:41:57 -0800

    Testing firebase hosted image

--- shopify-react/src/components/Navbar/Navbar.js
+++ shopify-react/src/components/Navbar/Navbar.js
@@ -34,7 +34,7 @@ const MyNav = () => {
     <Navbar expanded={expanded} onToggle={(e) => {if(e) setScrollState("#FFFFFF")}} style={{backgroundColor: scrollState}} className="justify-content-around" fixed="top" variant="light" expand="lg">
       <Navbar.Toggle onClick={()=> {setExpanded(!expanded)}} aria-controls="basic-navbar-nav" />
       <Link to="/">
-        <img className="brand" src={"https://i.ibb.co/B6T1b2n/Cloudy-Happiness-5.png"} alt="Cloudy Happiness" />
+        <img className="brand" src={"https://firebasestorage.googleapis.com/v0/b/cloudy-happiness-webstore.appspot.com/o/assets%2FCloudy-Happiness-5.png?alt=media&token=848adcab-b1f6-4c52-8fa7-1c6c3f41a475"} alt="Cloudy Happiness" />
       </Link>
       <div style={{display: "flex", width: "50px", justifyContent: "flex-end"}}>
         {/* <Nav.Link class="nav-icons ms-auto"><i class="fas fa-user"></i></Nav.Link> */}
