982625eb5b84bfd9ebf0714ffa77a7880e795e1a
fnyaung <fnyaung@gmail.com>
2021-11-29 11:34:09 -0800

    fixed size and quantity

--- shopify-react/src/components/SingleProduct/ProductImage.js
+++ shopify-react/src/components/SingleProduct/ProductImage.js
@@ -1,28 +1,46 @@
 import { Container, Row, Col } from "react-bootstrap";
-import {useState} from "react";
+import { useState } from "react";
 
 const ProductImage = (props) => {
-    const [currentImage, setCurrentImage] = useState(0);
+  const [currentImage, setCurrentImage] = useState(0);
 
-    return (
+  return (
+    <Row>
+      <Col>
         <Row>
-            <Col>
-                <Row>
-                    {props.images.map((e, index) =>{ 
-                        if(index > 3) return;
-                        return(
-                            <Col xs={12} className="pb-2" onClick={(event) => {setCurrentImage(index)}}>
-                                <img id="product-image" style={{width: "100%", border: currentImage == index ? '5px solid rgba(0, 0, 0, 0.3)' : ""}} src={props.images[index].src} />
-                            </Col>
-                        )
-                    })}
-                </Row>
-            </Col>
-            <Col xs={9}>
-                <img style={{width: "100%"}} src={props.images[currentImage].src} />
-            </Col>
+          {props.images.map((e, index) => {
+            if (index > 3) return;
+            return (
+              <Col
+                key={e.id}
+                xs={12}
+                className="pb-2"
+                onClick={(event) => {
+                  setCurrentImage(index);
+                }}
+              >
+                <img
+                  id="product-image"
+                  style={{
+                    width: "100%",
+                    border:
+                      currentImage == index
+                        ? "5px solid rgba(0, 0, 0, 0.3)"
+                        : "",
+                  }}
+                  alt={e.altText}
+                  src={e.src}
+                />
+              </Col>
+            );
+          })}
         </Row>
-    )
-}
+      </Col>
+      <Col xs={9}>
+        <img style={{ width: "100%" }} src={props.images[currentImage].src} />
+      </Col>
+    </Row>
+  );
+};
 
 export default ProductImage;
--- shopify-react/src/components/SingleProduct/SingleProduct.js
+++ shopify-react/src/components/SingleProduct/SingleProduct.js
@@ -1,24 +1,54 @@
-import React, { useEffect, useContext } from "react";
-import { Container, Row, Col, Button, InputGroup, FormControl } from "react-bootstrap";
+import React, { useEffect, useContext, useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../../context/shopContext";
 import ProductImage from "./ProductImage";
 
 import CloudyNav from "../Template/CloudyNav";
-import './SingleProduct.css';
+import "./SingleProduct.css";
 
 const ProductPage = () => {
   let { id } = useParams();
   const { fetchProductWithId, addItemToCheckout, product } =
     useContext(ShopContext);
+  const [quantity, setQuantity] = useState(0);
+  const [size, setSize] = useState(0);
   // const [product, setProduct] = useState(null)
   // async function fetchData() {
   //     const fetchedProduct = await fetchProductWithId(id)
   //     setProduct(fetchedProduct)
   // }
 
+  const onAddQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+  const onSubtractQuantity = () => {
+    if (quantity !== 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+  const onSetSize = (e) => {
+    let sizeClicked = e.target.innerHTML;
+    if (sizeClicked === "S") {
+      setSize(0);
+    } else if (sizeClicked === "M") {
+      setSize(1);
+    } else if (sizeClicked === "L") {
+      setSize(2);
+    } else if (sizeClicked === "XL") {
+      setSize(3);
+    }
+  };
+
   useEffect(() => {
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
     fetchProductWithId(id);
     // fetchData()
     return () => {
@@ -30,45 +60,80 @@ const ProductPage = () => {
   return (
     <>
       <CloudyNav />
-      <Container style={{fontFamily: 'Shrikhand'}}>
-          <ProductImage images={product.images} />
-          <Row className="pt-5">
-              <Col>
-                <h1>{product.title}</h1>
-                <h2>${product.variants[0].price}</h2>
-              </Col>
-          </Row>
-          <Row className="pt-3">
-              <Col><h3>Sizes</h3></Col>
-          </Row>
-          <Row style={{width: "20rem"}}>
-              <Col><Button className="white-button">S</Button></Col>
-              <Col><Button className="white-button">M</Button></Col>
-              <Col><Button className="white-button">L</Button></Col>
-              <Col><Button className="white-button">XL</Button></Col>
-          </Row>
-          <Row className="pt-4">
-              <Col>
-                <h3>Quantity</h3>
-                <InputGroup className="mb-3" style={{width: "130px"}}>
-                  <Button variant="outline-secondary" id="button-addon1">
-                    -
-                  </Button>
-                  <FormControl
-                    aria-label="Example text with button addon"
-                    aria-describedby="basic-addon1"
-                  />
-                  <Button variant="outline-secondary" id="button-addon2">
-                    +
-                  </Button>
-                </InputGroup>
-              </Col>
-          </Row>
-          <Row className="pt-4 text-center">
-              <Col>
-                <Button className="white-button rounded-pill" onClick={() => addItemToCheckout(product.variants[0].id, 1)}>Add to Cart</Button>
-              </Col>
-          </Row>
+      <Container style={{ fontFamily: "Shrikhand" }}>
+        <ProductImage images={product.images} />
+        <Row className="pt-5">
+          <Col>
+            <h1>{product.title}</h1>
+            <h2>${product.variants[0].price}</h2>
+          </Col>
+        </Row>
+        <Row className="pt-3">
+          <Col>
+            <h3>Sizes</h3>
+          </Col>
+        </Row>
+        <Row style={{ width: "20rem" }}>
+          <Col>
+            <Button className="white-button" onClick={onSetSize}>
+              S
+            </Button>
+          </Col>
+          <Col>
+            <Button className="white-button" onClick={onSetSize}>
+              M
+            </Button>
+          </Col>
+          <Col>
+            <Button className="white-button" onClick={onSetSize}>
+              L
+            </Button>
+          </Col>
+          <Col>
+            <Button className="white-button" onClick={onSetSize}>
+              XL
+            </Button>
+          </Col>
+        </Row>
+        <Row className="pt-4">
+          <Col>
+            <h3>Quantity</h3>
+            <InputGroup className="mb-3" style={{ width: "130px" }}>
+              <Button
+                variant="outline-secondary"
+                id="button-addon1"
+                onClick={onSubtractQuantity}
+              >
+                -
+              </Button>
+              <FormControl
+                aria-label="Example text with button addon"
+                aria-describedby="basic-addon1"
+                type="number"
+                value={quantity}
+              />
+              <Button
+                variant="outline-secondary"
+                id="button-addon2"
+                onClick={onAddQuantity}
+              >
+                +
+              </Button>
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row className="pt-4 text-center">
+          <Col>
+            <Button
+              className="white-button rounded-pill"
+              onClick={() =>
+                addItemToCheckout(product.variants[size].id, quantity)
+              }
+            >
+              Add to Cart
+            </Button>
+          </Col>
+        </Row>
       </Container>
     </>
   );
